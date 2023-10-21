aa752129c20a7146d8849005c9bd938d832b2cc6
thegecko <rob.moran@arm.com>
2023-10-21 08:40:22 +0100

    Authors & license

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,6 +1,9 @@
-/**
- * Copyright (C) 2023 Arm Limited
- */
+/********************************************************************************
+ * Copyright (C) 2023 Marcel Ball, Arm Limited and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the MIT License as outlined in the LICENSE File
+ ********************************************************************************/
 
 export const PACKAGE_NAME = 'svd-viewer';
 export const CONFIG_SVD_PATH = 'svdPathConfig';
