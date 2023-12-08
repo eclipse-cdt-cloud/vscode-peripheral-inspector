6d814f80c3ef0baa2b9d8b720d5203b8a4565307
Asim Gunes <asim.gunes@smartbee.uk>
2023-12-08 09:41:46 +0000

    Minor changes related to the suggestions from code review

--- src/api-types.ts
+++ src/api-types.ts
@@ -1,83 +1,83 @@
 // Peripheral Inspector API
 export interface IPeripheralInspectorAPI {
-	registerSVDFile: (expression: RegExp | string, path: string) => void;
-	getSVDFile: (device: string) => string | undefined;
-	getSVDFileFromCortexDebug: (device: string) => Promise<string | undefined>;
-	registerPeripheralsProvider: (fileExtension: string, provider: IPeripheralsProvider) => void;
+    registerSVDFile: (expression: RegExp | string, path: string) => void;
+    getSVDFile: (device: string) => string | undefined;
+    getSVDFileFromCortexDebug: (device: string) => Promise<string | undefined>;
+    registerPeripheralsProvider: (fileExtension: string, provider: IPeripheralsProvider) => void;
 }
 
 export interface IPeripheralsProvider {
-	getPeripherals: (data: string, options: IGetPeripheralsArguments) => Promise<PeripheralOptions[]>;
+    getPeripherals: (data: string, options: IGetPeripheralsArguments) => Promise<PeripheralOptions[]>;
 }
 
 export interface IGetPeripheralsArguments {
-	gapThreshold: number;
+    gapThreshold: number;
 }
 
 export interface PeripheralOptions {
-	name: string;
-	baseAddress: number;
-	totalLength: number;
-	description: string;
-	groupName?: string;
-	accessType?: AccessType;
-	size?: number;
-	resetValue?: number;
-	registers?: PeripheralRegisterOptions[];
-	clusters?: ClusterOptions[];
+    name: string;
+    baseAddress: number;
+    totalLength: number;
+    description: string;
+    groupName?: string;
+    accessType?: AccessType;
+    size?: number;
+    resetValue?: number;
+    registers?: PeripheralRegisterOptions[];
+    clusters?: ClusterOptions[];
 }
 
 export interface PeripheralRegisterOptions {
-	name: string;
-	description?: string;
-	addressOffset: number;
-	accessType?: AccessType;
-	size?: number;
-	resetValue?: number;
-	fields?: FieldOptions[];
+    name: string;
+    description?: string;
+    addressOffset: number;
+    accessType?: AccessType;
+    size?: number;
+    resetValue?: number;
+    fields?: FieldOptions[];
 }
 
 export interface ClusterOptions {
-	name: string;
-	description?: string;
-	addressOffset: number;
-	accessType?: AccessType;
-	size?: number;
-	resetValue?: number;
-	registers?: PeripheralRegisterOptions[];
-	clusters?: ClusterOptions[];
+    name: string;
+    description?: string;
+    addressOffset: number;
+    accessType?: AccessType;
+    size?: number;
+    resetValue?: number;
+    registers?: PeripheralRegisterOptions[];
+    clusters?: ClusterOptions[];
 }
 
 export interface FieldOptions {
-	name: string;
-	description: string;
-	offset: number;
-	width: number;
-	enumeration?: EnumerationMap;
-	derivedFrom?: string;           // Set this if unresolved
-	accessType?: AccessType;
+    name: string;
+    description: string;
+    offset: number;
+    width: number;
+    enumeration?: EnumerationMap;
+    derivedFrom?: string;           // Set this if unresolved
+    accessType?: AccessType;
 }
 
 export interface IGetPeripheralsArguments {
-	gapThreshold: number;
+    gapThreshold: number;
 }
 
 export interface IPeripheralsProvider {
-	getPeripherals: (data: string, options: IGetPeripheralsArguments) => Promise<PeripheralOptions[]>;
+    getPeripherals: (data: string, options: IGetPeripheralsArguments) => Promise<PeripheralOptions[]>;
 }
 
-export declare enum AccessType {
+export enum AccessType {
     ReadOnly = 1,
     ReadWrite = 2,
     WriteOnly = 3
 }
 
 export interface EnumerationMap {
-	[value: number]: IEnumeratedValue;
+    [value: number]: IEnumeratedValue;
 }
 
 export interface IEnumeratedValue {
-	name: string;
-	description: string;
-	value: number;
+    name: string;
+    description: string;
+    value: number;
 }
